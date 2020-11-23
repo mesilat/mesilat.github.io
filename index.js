d8400af770be8b1740a63a89fe8f00df5d1fc1e4
vb-mesilat <vb@mesilat.com>
2020-11-23 11:17:54 +0300

    Updates to Confluence Fields plugin docs

--- index.js
+++ index.js
@@ -1,10 +1,95 @@
 const { join } = require("path");
-const { mkdirSync, writeFileSync, copyFileSync } = require("fs");
-const { readdir } = require('fs').promises;
+const {
+  mkdirSync, writeFileSync, copyFileSync, createWriteStream
+} = require("fs");
+const { copySync } = require("fs-extra");
+const { readdir } = require("fs").promises;
 const compressor = require("yuicompressor");
+const https = require("https");
+const axios = require("axios");
+const rimraf = require("rimraf");
+const unzipper = require("unzipper");
 
 const charset = "utf8";
 const target = "docs";
+
+const baseurl = process.env.PERS_WIKI_HOME;
+const username = process.env.PERS_WIKI_USER;
+const password = process.env.PERS_WIKI_PASSWORD;
+const rootPageId = "44696055";
+
+axios.defaults.httpsAgent = new https.Agent({ rejectUnauthorized: false });
+axios.defaults.auth = { username, password };
+axios.defaults.headers = { 'x-atlassian-token': 'no-check' };
+
+const exportSettings = {
+  exportScheme: {
+    id: 'bundled_default',
+    name: 'This page and children as WebHelp',
+    pageSelectionStrategy: {
+      completeKey: 'com.k15t.scroll.scroll-html:alldescendants',
+      properties: {}
+    },
+    pageBuilder: {
+      processTocMacros: true,
+      processChildrenMacros: true,
+      enableHeadingPromotion: true,
+      processSectionColumnMacroAsTable: false,
+      processNumberedHeadingsMacros: false,
+      embedAttachmentsOption: 'notEmbedAttachments'
+    },
+    exporter: {
+      exporterId: 'com.k15t.scroll.scroll-html',
+      templateId: 'com.k15t.scroll.scroll-html:default-template',
+      properties: {
+        buildSearchIndex: 'true',
+        exportMode: 'default',
+        "linkNamingStrategy.extendedCharHandling": 'Ignore',
+        "linkNamingStrategy.whitespaceHandling": 'Underscore',
+        "linkNamingStrategy.fileNameSchema": 'PageId',
+        "linkNamingStrategy.extension": 'html'
+      }
+    },
+    exportAdhocPublishedOnly: false
+  },
+  rootPageId
+};
+
+async function startExport() {
+  return axios.put(`${baseurl}/rest/scroll-html/1.0/export`, exportSettings);
+}
+
+async function checkStatus(exportId) {
+  return axios.get(`${baseurl}/rest/scroll-html/1.0/async-tasks/${exportId}`);
+}
+
+async function downloadFile(exportId, filename) {
+  console.debug(`Downloading ${baseurl}/rest/scroll-html/1.0/export/${exportId}/${encodeURIComponent(filename)}`);
+  const resp = await axios.get(`${baseurl}/rest/scroll-html/1.0/export/${exportId}/${encodeURIComponent(filename)}`, {
+    responseType: 'stream'
+  });
+  await resp.data.pipe(unzipper.Extract({ path: 'src/' }));
+  // await resp.data.pipe(createWriteStream(filename));
+  // await createReadStream(filename).pipe(unzip.Extract({ path: 'src/' }));
+}
+
+const sleep = async (ms) => new Promise(resolve => setTimeout(() => resolve(), ms));
+
+async function exportPages() {
+  const resp = await startExport();
+  const exportId = resp.data.id;
+
+  let status;
+  do {
+    await sleep(1000);
+    status = await checkStatus(exportId);
+    console.debug(`Progress: ${status.data.progress}`);
+  } while (!status.data.filename);
+
+  rimraf.sync('src/*');
+  await downloadFile(exportId, status.data.filename);
+  await sleep(1000);
+}
 
 async function listFiles(dir) {
   const dirents = await readdir(dir, { withFileTypes: true });
@@ -60,6 +145,10 @@ function logError(text) {
 }
 
 async function main() {
+  // Export the space
+  await exportPages();
+
+  // Process all files
   const files = await listFiles("src");
   for (let i = 0; i < files.length; i++) {
     const file = files[i];
@@ -84,6 +173,9 @@ async function main() {
       logSuccess(file);
     }
   }
+
+  // Copy redirects
+  copySync('redirects', 'src');
 }
 
 main();
